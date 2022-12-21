b51f46c74427a582b759518e6c898b8066414568
Samantha Le Huec <s.lhuec@gmail.com>
2022-12-21 15:05:20 +0100

    makefile

--- ex00/easyfind.hpp
+++ ex00/easyfind.hpp
@@ -1,13 +1,13 @@
 #ifndef _EASYFIND_HPP
 #define _EASYFIND_HPP
 
-#include <iostream>
 #include <algorithm>
 
 template<typename T>
-void easyfind(T& container_type, unsigned int n)
+T::iterator easyfind(T& container_type, unsigned int n)
 {
 	std::find(container_type.begin(), container_type.end(), n)
+	
 }
 
 #endif
